15e0ab61e49a291067f17a6e79ca1f59b98b73d8
Marcos Diez <marcos@unitron.com.br>
2023-05-21 18:17:49 -0300

    wow

--- ofx_bradesco_to_json2.py
+++ ofx_bradesco_to_json2.py
@@ -13,7 +13,7 @@ def yield_every_char_from_file(filename):
 
 
 uninteresting_chars = ["\n", "\t", "\r"]
-tags_that_must_be_lists = ["STMTTRN"]
+tags_that_must_be_lists = ["STMTTRN", "/STMTTRN"]
 
 def dumper(obj):
     print(json.dumps(obj, sort_keys=False, indent=2))
@@ -31,10 +31,12 @@ def add_to_result(result, tag_stack, value):
 
 
 def process_ofx(filename):
+    counter = 0
     result = {}
     tag_stack = []
     next_char = None
     current_string = ""
+    previsous_string = ""
     value_mode = False
     for _char in yield_every_char_from_file(filename):
         if _char in uninteresting_chars:
@@ -51,16 +53,25 @@ def process_ofx(filename):
                 value = current_string
                 add_to_result(result, tag_stack, value)
                 tag_stack.pop()
+            previsous_string = current_string
             current_string = ""
             continue
 
         if char == ">":
             if current_string.startswith("/"):
                 value_mode = False
+                # print(f"---- {current_string} {previsous_string}")
+
+                previsous_string = current_string
                 current_string = ""
                 tag_stack.pop()
+                if previsous_string in tags_that_must_be_lists:
+                    tag_stack.pop()
             else:
                 tag_stack.append(current_string)
+                if current_string in tags_that_must_be_lists:
+                    tag_stack.append(counter)
+                    counter+=1
                 value_mode = next_char != "<"
                 current_string = ""
             continue
