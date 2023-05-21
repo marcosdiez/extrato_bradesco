7021bc5e8bd2a44dfa179fbd88a856dc6ca62174
Marcos Diez <marcos@unitron.com.br>
2023-05-21 18:12:03 -0300

    better, yet still wrong

--- ofx_bradesco_to_json2.py
+++ ofx_bradesco_to_json2.py
@@ -2,6 +2,7 @@
 import sys
 import json
 
+
 def yield_every_char_from_file(filename):
     with open(filename) as stream:
         while True:
@@ -12,9 +13,22 @@ def yield_every_char_from_file(filename):
 
 
 uninteresting_chars = ["\n", "\t", "\r"]
+tags_that_must_be_lists = ["STMTTRN"]
+
+def dumper(obj):
+    print(json.dumps(obj, sort_keys=False, indent=2))
+
 
 def add_to_result(result, tag_stack, value):
     print(f"{tag_stack} = {value}")
+    current_position = result
+    for tag in tag_stack[0:-1]:
+        if tag not in current_position:
+            current_position[tag] = {}
+        current_position = current_position[tag]
+    last_tag = tag_stack[-1]
+    current_position[last_tag] = value
+
 
 def process_ofx(filename):
     result = {}
@@ -43,7 +57,7 @@ def process_ofx(filename):
         if char == ">":
             if current_string.startswith("/"):
                 value_mode = False
-                current_string=""
+                current_string = ""
                 tag_stack.pop()
             else:
                 tag_stack.append(current_string)
@@ -51,6 +65,8 @@ def process_ofx(filename):
                 current_string = ""
             continue
         current_string += char
+    return result
 
-process_ofx(sys.argv[1])
 
+result = process_ofx(sys.argv[1])
+dumper(result)
