8ad86b0b0512c9a71a8e78a98606e1c231ca5ca3
Marcos Diez <marcos@unitron.com.br>
2023-05-22 08:56:19 -0300

    black

--- ofx_bradesco_to_json2.py
+++ ofx_bradesco_to_json2.py
@@ -3,6 +3,7 @@ import sys
 import json
 import os
 
+
 def yield_every_char_from_file(filename):
     with open(filename) as stream:
         while True:
@@ -14,6 +15,7 @@ def yield_every_char_from_file(filename):
 
 uninteresting_chars = ["\n", "\t", "\r"]
 tags_that_must_be_lists = ["STMTTRN"]
+
 
 def dumper(obj):
     print(json.dumps(obj, sort_keys=False, indent=2))
@@ -31,20 +33,21 @@ def add_to_result(result, tag_stack, value):
 
 
 def fix_stmtrn(result):
-    bank_trans_list = result["OFX"]["BANKMSGSRSV1"]["STMTTRNRS"]["STMTRS"]["BANKTRANLIST"]
+    bank_trans_list = result["OFX"]["BANKMSGSRSV1"]["STMTTRNRS"]["STMTRS"][
+        "BANKTRANLIST"
+    ]
     new_stmttrn = []
 
-    for key , value in bank_trans_list["STMTTRN"].items():
+    for key, value in bank_trans_list["STMTTRN"].items():
         new_stmttrn.append(value)
 
     bank_trans_list["STMTTRN"] = new_stmttrn
     return result
 
+
 def process_ofx(filename):
     counter = 0
-    result = {
-        "_header": {}
-    }
+    result = {"_header": {}}
     tag_stack = []
     next_char = None
     char = None
@@ -75,7 +78,7 @@ def process_ofx(filename):
                 header_mode = False
                 continue
             else:
-                 current_string += char
+                current_string += char
 
         else:
             if _char in uninteresting_chars:
@@ -108,22 +111,28 @@ def process_ofx(filename):
                     tag_stack.append(current_string)
                     if current_string in tags_that_must_be_lists:
                         tag_stack.append(counter)
-                        counter+=1
+                        counter += 1
                     value_mode = next_char != "<"
                     current_string = ""
                 continue
             current_string += char
     return result
 
+
 def _get_target_filename(input_file_name):
     if len(sys.argv) <= 2:
-        return input_file_name[0:input_file_name.rfind(".")] + ".json"
+        return input_file_name[0 : input_file_name.rfind(".")] + ".json"
     else:
         return sys.argv[2]
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2 or not os.path.isfile(sys.argv[1]):
-        print("usage: {} ofx_to_become_a_json.ofx [optional_output_name.json]".format(sys.argv[0]))
+        print(
+            "usage: {} ofx_to_become_a_json.ofx [optional_output_name.json]".format(
+                sys.argv[0]
+            )
+        )
         sys.exit(1)
 
     input_filename = sys.argv[1]
